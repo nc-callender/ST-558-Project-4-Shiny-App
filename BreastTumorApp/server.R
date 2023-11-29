2292851977181468e8fb8e40b33cda62f0b4b7d7
nc-callender <eddieandrockyarecats.gmail.com>
2023-11-29 13:26:44 -0500

    Finish draft numerical summary

--- BreastTumorApp/server.R
+++ BreastTumorApp/server.R
@@ -9,10 +9,97 @@
 
 library(shiny)
 library(shinydashboard)
+library(DT)
+library(plotly)
+library(tidyverse)
 
-# Define server logic required to draw a histogram
-function(input, output, session) {
+#Import Data Set
+tumor_data <- read.csv("brca.csv")
+
+#Make a lookup table for variable names relating to characteristic and dimension
+variable_names <- as_tibble(colnames(tumor_data)) 
+colnames(variable_names) <- c("original_name")
+variable_names <- variable_names %>% filter ((original_name != "X" & original_name != "y"))
+
+characteristic_base <- c("Radius", "Texture", "Perimeter", "Area", "Smoothness", "Compactness", "Concavity", "Concave Points", "Symmetry", "Fractal Dimension")
+characteristic <- rep(characteristic_base,times  =3)
+
+dimension_base <-c ("Mean", "Std Error", "Worst")
+dimension <- rep(dimension_base, times = 10)
+
+variable_table <- data.frame(variable_names, characteristic, dimension )
+
+# Begin server code
+shinyServer(function(input, output, session) {
+
+# Output for numeric summary tab
+    output$numeric_summary <- renderTable({
+        #get variable name
+        variable_for_summ <- variable_table %>% 
+            filter(characteristic == input$char_of_interest_ns) %>%
+            filter (dimension == input$dim_of_interest_ns) %>%
+            select (original_name) %>%
+            mutate (y = "y")
+
+        #Select variable name and y, label y for plotting
+        table_ns <- tumor_data %>% 
+            select_({{variable_for_summ[1,1]}},{{variable_for_summ[1,2]}}) 
+            colnames(table_ns) <- c("x","Diagnosis")
+        table_ns$Diagnosis <-factor(table_ns$Diagnosis, levels=c("B", "M"), 
+                            labels = c("Benign", "Malignant"))
+        #Make table according to if filtering desired, grouping desired and what summary is requested.
+        if(input$filtering_ns=="Malignant and Benign"){
+            if(input$grouping){
+                if(input$type_of_summary == "Min, Median, Max"){
+                    table_ns <- table_ns %>% 
+                        group_by(Diagnosis) %>%
+                        summarize (Min = min(x), Median = median(x), Max = max(x))
+                }
+                else{
+                    table_ns <- table_ns %>% 
+                        group_by(Diagnosis) %>%
+                        summarize (Mean = round(mean(x),3), "Std. Dev." = round(sd(x),3))
+                }
+            } #end for no filtering but with grouping desired
+            else{
+               if(input$type_of_summary == "Min, Median, Max"){
+                   table_ns <- table_ns %>% 
+                   summarize (Min = min(x), Median = median(x), Max = max(x))}
+                else{
+                    table_ns <- table_ns %>% 
+                    summarize (Mean = round(mean(x),3), "Std. Dev." = round(sd(x),3))
+                }
+            }#end for no filter no group
+        }#end for no filter
+        if(input$filtering_ns=="Malignant" | input$filtering_ns=="Benign"){
+            if(input$type_of_summary == "Min, Median, Max"){
+                table_ns <- table_ns %>% 
+                    filter(Diagnosis == input$filtering_ns) %>%
+                    summarize (Min = min(x), Median = median(x), Max = max(x))
+            }
+            else{
+              table_ns <- table_ns %>% 
+                filter(Diagnosis == input$filtering_ns) %>%
+                summarize (Mean = round(mean(x),3), "Std. Dev." = round(sd(x),3))
+            }
+          } #end for filtering 
+
+table_ns 
+})  # end of numeric summary section
+  
+      output$test_text <- renderText({
+        #get variable name
+        variable_for_summ <- variable_table %>% 
+            filter(characteristic == input$char_of_interest_ns) %>%
+            filter (dimension == input$dim_of_interest_ns) %>%
+            select (original_name)
+#        
+        paste("The variable is", variable_for_summ)
+#          variable_for_summ
+#        table <- summary(variable_for_summ)
+#        table
+      
+    })
 
 
-
-}
+})
--- BreastTumorApp/ui.R
+++ BreastTumorApp/ui.R
@@ -8,7 +8,8 @@ library(plotly)
 ui<-dashboardPage(skin = "blue",
   
     # Application title
-    dashboardHeader(title = "Breast Tumor Charateristics and Modeling of Malignancy"),
+    dashboardHeader(title = "Breast Tumor Charateristics and Modeling of Malignancy",
+                    titleWidth=600),
 
     # Sidebar 
     dashboardSidebar(
@@ -23,16 +24,64 @@ ui<-dashboardPage(skin = "blue",
     # Main Section 
     dashboardBody(
         tabItems(
+          
+#Tab 1- About
             tabItem("about",
-                fluidPage(h1("Header"))),
+                fluidPage(p("The purpose of this app is to allow the user to investigate the characteristics of cells from breast tumors. The user can investigate how the characteristics relate to each other and how they relate to whether the tumor is benign or malignant. "),
+                  br(),
+                  a(href = "https://www.kaggle.com/datasets/utkarshx27/breast-cancer-wisconsin-diagnostic-dataset","The dataset entitled  “Breast Cancer Wisconsin Diagnostic Dataset” was downloaded from here."),
+                  br(),
+                  p("The dataset contains columns related to the radius, perimeter, area, smoothness, compactness, concavity, number of concave point, symmetry and fractal dimension.  For each of these characteristics., the mean, standard error, and “worst” measurement are available.  The dataset also contains a factor denoting whether the mass was malignant or benign."),
+                  br(),
+                  
+                  p("There are three main tabs available:"),
+                  p("About:  Describes the characteristics of the data and app."),
+                  p("Data Exploration:  Allows the user to make numerical summaries and graphs to investigate how the variables interact with each other and with whether the tumor was benign or malignant."),
+                  p("Modelling:  Allows the user to model the data using generalize linear regression or random forest.  "),
+                  br(),
+                  tags$img(src = "pinkribbon.jpg", width = 160), 
+                  tags$img(src = "pinkribbon.jpg", width = 160),
+                  tags$img(src = "pinkribbon.jpg", width = 160)
+                          ),
+                
+                ),
+#Tab2 Data Exploration
             tabItem("data_exploration",
-                fluidPage(h1("data explore"))),
+                tabsetPanel(
+                    tabPanel("Numeric Summaries",
+                        sidebarPanel(
+                           selectInput ("char_of_interest_ns", 
+                                        "Select a characteristic for summarization.",
+                                        c("Radius", "Texture", "Perimeter", "Area", "Smoothness",
+                                          "Compactness", "Concavity", "Concave Points", "Symmetry",
+                                          "Fractal Dimension")),
+                           selectInput ("dim_of_interest_ns", 
+                                        "Select a parameter for summarization",
+                                        c("Mean", "Std Error", "Worst")),
+                           selectInput("type_of_summary",
+                                       "Select a type of summary.", 
+                                       c("Min, Median, Max", "Mean and Std Dev")),
+                           selectInput("filtering_ns",
+                                       "Select whether to filter the data.",
+                                       c("Malignant and Benign", "Malignant", "Benign")),
+                           conditionalPanel(condition="input.filtering_ns == 'Malignant and Benign'",
+                              checkboxInput("grouping",
+                                 "Do you want to group by malignant versus benign?")),
+                           width = 5),
+                        mainPanel(box(tableOutput("numeric_summary")), width=12)
+                    ), #End of numeric summary panel
+                    tabPanel("Grapical Summaries",fluidPage(h1("datagraph")))
+                ),
+            ),
             tabItem("modeling",
-                fluidPage(h1 ("model")))
-        )
-        
-    
+                tabsetPanel(
+                    tabPanel("Modeling Info",fluidPage(h1("model info"))),
+                    tabPanel("Model Fitting", fluidPage(h1("model fitting"))),
+                    tabPanel("Prediction", fluidPage(h1("model predict")))
+                )
+                
+            )
     )
     
 )
-        
+)        
